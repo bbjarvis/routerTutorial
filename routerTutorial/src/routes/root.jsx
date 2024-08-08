f33ae57dd1bc7806b4e27e23cce6df0ca22873b5
Brett Jarvis <brettbjarvis@gmail.com>
2024-08-08 11:48:44 -0700

    use useNavigation for isLoading graphic

--- routerTutorial/src/routes/root.jsx
+++ routerTutorial/src/routes/root.jsx
@@ -5,6 +5,7 @@ import {
   Form,
   redirect,
   NavLink,
+  useNavigation,
 } from 'react-router-dom'
 import { getContacts, createContact } from '../contacts'
 
@@ -21,6 +22,7 @@ export async function loader() {
 
 export default function Root() {
   const { contacts } = useLoaderData()
+  const navigation = useNavigation()
   return (
     <>
       <div id='sidebar'>
@@ -71,7 +73,10 @@ export default function Root() {
           )}
         </nav>
       </div>
-      <div id='detail'>
+      <div
+        id='detail'
+        className={navigation.state === 'loading' ? 'loading' : ''}
+      >
         <Outlet />
       </div>
     </>
